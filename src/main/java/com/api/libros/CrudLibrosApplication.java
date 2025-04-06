9e792bcc28bf7f6f36cd112509c0881430fe4fd4
jplara182001 <jplarar1@academia.usbbog.edu.co>
2025-04-06 22:40:28 +0000

    Importación Spring initializr

---
+++ src/main/java/com/api/libros/CrudLibrosApplication.java
@@ -0,0 +1,13 @@
+package com.api.libros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudLibrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudLibrosApplication.class, args);
+	}
+
+}
